ae4181608a189dac85047c666fb5499ed7e9e21a
Matias Ariel Larrosa Babio <69860562+Espurna01@users.noreply.github.com>
2022-03-07 22:43:45 +0100

    finish

--- main.c
+++ main.c
@@ -3,48 +3,83 @@
 #include <time.h>
 #define MAXRGB 256
 
-void savePPM(char *filename, int size){
+void saveSpiralPPM(char *filename, int size){
     FILE *file = fopen(filename, "wb");
-    if(file == NULL){
+
+    if(file == NULL)
         return;
-    }
+
+    char R[size / 2];
+    for(int i = 0; i < size / 2;i++)
+        R[i] = rand() % MAXRGB;
+
+    char G[size / 2];
+    for(int i = 0; i < size / 2;i++)
+        G[i] = rand() % MAXRGB;
+
+    char B[size / 2];
+    for(int i = 0; i < size / 2;i++)
+        B[i] = rand() % MAXRGB;
+
     fprintf(file, "P6 %d %d %d\n", size, size, MAXRGB - 1);
-    for(int i = 0;i < size;i++){
+    for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            int r;
-            r = 255;
-            //fwrite(&colors[r], 1, 1, file);
-            //fwrite(&colors[r], 1, 1, file);
-            //fwrite(&colors[r], 1, 1, file);
-            if(i == j || size - i - 1 == j) r = 0;
-            fwrite(&r, 1, 1, file);
-            fwrite(&r, 1, 1, file);
-            fwrite(&r, 1, 1, file);
+            int depth = 0;
+            while(i != depth && j != depth &&
+                  i != size - depth - 1 && j != size - depth - 1){
+                depth++;
+            }
+            fwrite(&R[depth], 1, 1, file);
+            fwrite(&G[depth], 1, 1, file);
+            fwrite(&B[depth], 1, 1, file);
         }
     }
+
+}
+
+void savePPMTest(char *filename, int size){
+    FILE *file = fopen(filename, "wb");
+
+    if(file == NULL)
+        return;
+
+    char R[size / 2];
+    for(int i = 0; i < size / 2;i++)
+        R[i] = rand() % MAXRGB;
+
+//    char G[size / 2];
+//    for(int i = 0; i < size / 2;i++)
+//        G[i] = rand() % MAXRGB;
+//
+//    char B[size / 2];
+//    for(int i = 0; i < size / 2;i++)
+//        B[i] = rand() % MAXRGB;
+
+    fprintf(file, "P6 %d %d %d\n", size, size, MAXRGB - 1);
+    for(int i = 0; i < size * 3; i++){
+        for(int j = 0; j < size; j++){
+            int depth = 0;
+            int vali = i;
+            i = i % size;
+            while(i != depth && j != depth &&
+                  i != size - depth - 1 && j != size - depth - 1){
+                depth++;
+            }
+            fwrite(&R[depth], 1, 1, file);
+            i = vali;
+        }
+        printf("\n");
+    }
+
 }
 
 int main()
 {
     srand(time(NULL));
-//    savePPM("prueba1.ppm", 100);
-//    savePPM("prueba2.ppm", 80);
-//    savePPM("prueba3.ppm", 60);
-//    savePPM("prueba4.ppm", 40);
-//    savePPM("prueba5.ppm", 20);
-//    savePPM("prueba6.ppm", 10);
-//    savePPM("prueba7.ppm", 8);
-//    savePPM("prueba8.ppm", 6);
-//    savePPM("prueba9.ppm", 4);
-//    savePPM("prueba10.ppm", 2);
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            if(j != 0) printf("\t");
-            //int val = i * j;
-            //if(i + j > 10) val = (10 - i) * (10 - j) ;
-            printf("%d", (i + j)/10);
-        }
-        printf("\n");
-    }
+//    saveSpiralPPM("pruebaEspiral100x100.ppm", 100);
+//    saveSpiralPPM("pruebaEspiral10x10.ppm", 10);
+//    saveSpiralPPM("pruebaEspiral1000x1000.ppm", 1000);
+//    saveSpiralPPM("32x32.ppm", 32);
+    savePPMTest("pruebaTest.ppm", 50);
     return 0;
 }
