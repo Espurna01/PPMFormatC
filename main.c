26222266a57c30aedcaf9debd9cf724239d46efe
Matias Ariel Larrosa Babio <69860562+Espurna01@users.noreply.github.com>
2022-03-10 06:59:47 +0100

    code for multipleSpiral working
    was missing ()

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #define MAXRGB 256
-#define MINSLICES 2
+#define MINSLICES 5
 
 void saveSpiralPPM(char *filename, int size){
     FILE *file = fopen(filename, "wb");
@@ -38,26 +38,29 @@ void saveSpiralPPM(char *filename, int size){
     fclose(file);
 }
 
-void savePPMMultipleSpiral(char *filename, int size){
-    FILE *file = fopen(filename, "wb");
-
-    if(file == NULL)
-        return;
+int savePPMMultipleSpiral(char *filename, int size){
 
     int slices = MINSLICES;
     while(size % slices != 0 && slices < size)
         slices++;
+    if(size == slices) return -1;
+    int nColors = size / 2 / slices;
 
-    char R[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
+    FILE *file = fopen(filename, "wb");
+
+    if(file == NULL)
+        return -1;
+
+    char R[nColors];
+    for(int i = 0; i < nColors;i++)
         R[i] = rand() % MAXRGB;
 
-    char G[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
+    char G[nColors];
+    for(int i = 0; i < nColors;i++)
         G[i] = rand() % MAXRGB;
 
-    char B[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
+    char B[nColors];
+    for(int i = 0; i < nColors;i++)
         B[i] = rand() % MAXRGB;
 
     fprintf(file, "P6 %d %d %d\n", size, size, MAXRGB - 1);
@@ -69,67 +72,26 @@ void savePPMMultipleSpiral(char *filename, int size){
                 pos++;
             }
             int depth = 0;
-            while(i != depth + size/slices * pos/slices && j != (depth + size/slices * pos % slices) &&
+            while(i != depth + size/slices * (pos/slices) && j != (depth + size/slices * (pos % slices)) &&
                 i != (size/slices * (pos / slices + 1) - 1 - depth) && j != size / slices * (pos % slices + 1) - 1 - depth){
                 depth++;
             }
+            if(pos % 2 == 0){
+                fwrite(&R[depth], 1, 1, file);
+                fwrite(&G[depth], 1, 1, file);
+                fwrite(&B[depth], 1, 1, file);
+            } else {
+                fwrite(&R[nColors - 1 - depth], 1, 1, file);
+                fwrite(&G[nColors - 1 - depth], 1, 1, file);
+                fwrite(&B[nColors - 1 - depth], 1, 1, file);
+            }
 
-            printf("pos(%d) = %d, ", pos, depth);
-            fwrite(&R[depth], 1, 1, file);
-            fwrite(&G[depth], 1, 1, file);
-            fwrite(&B[depth], 1, 1, file);
         }
-        printf("\n");
     }
 
 
     fclose(file);
-}
-
-void savePPMMultipleSpiral2(char *filename, int size){
-    FILE *file = fopen(filename, "wb");
-
-    if(file == NULL)
-        return;
-
-    int slices = MINSLICES;
-    while(size % slices != 0 && slices < size)
-        slices++;
-
-    char R[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
-        R[i] = rand() % MAXRGB;
-
-    char G[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
-        G[i] = rand() % MAXRGB;
-
-    char B[size / 2 / slices];
-    for(int i = 0; i < size / 2 / slices;i++)
-        B[i] = rand() % MAXRGB;
-
-    fprintf(file, "P2 %d %d %d\n", size, size, MAXRGB - 1);
-
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            int pos = 0;
-            while(i >= size/slices * (pos/slices + 1) || j >= size/slices * (pos % slices + 1)){
-                pos++;
-            }
-            int depth = 0;
-            while(i != depth + size/slices * pos/slices && j != (depth + size/slices * pos % slices) &&
-                i != (size/slices * (pos / slices + 1) - 1 - depth) && j != size / slices * (pos % slices + 1) - 1 - depth){
-                depth++;
-            }
-            printf("pos(%d) = %d, ", pos, depth);
-            fprintf(file, "%d\t", R[depth]);
-        }
-        printf("\n");
-        fprintf(file, "\n");
-    }
-
-
-    fclose(file);
+    return slices;
 }
 
 void savePPMTest(char *filename, int size){
@@ -177,9 +139,8 @@ int main()
 //    saveSpiralPPM("32x33.ppm", 32);
 //    savePPMMultipleSpiral2("test.ppm", 8, 2);
 
-    saveSpiralPPM("32x32.ppm", 32);
-    savePPMMultipleSpiral2("test.pgm", 8);
-
+    int s = savePPMMultipleSpiral("test.ppm", 100);
+    printf("%d piezas", s*s);
 
     return 0;
 }
